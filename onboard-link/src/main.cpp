c4c9ed1ba8ecdf0b17dadfc823ef5bdfa287fd7b
oliverdantzer <oliverdantzer@gmail.com>
2024-10-24 02:08:16 -0400

    Formatting, added arg to specify target address, edited constructor calls to conform to new definitions

--- onboard-link/src/main.cpp
+++ onboard-link/src/main.cpp
@@ -1,32 +1,33 @@
+#include "command.hpp"
+#include "recv_server.hpp"
+#include "send_server.hpp"
+#include "telemetry.hpp"
+#include <boost/asio.hpp>
 #include <iostream>
 #include <string>
-#include <boost/asio.hpp>
-#include "send_server.hpp"
-#include "recv_server.hpp"
-#include "telemetry.hpp"
-#include "command.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    try
-    {
-        if (argc != 4)
-        {
-            std::cerr << "Usage: " << argv[0] << " <recv_port> <send_port> <target_port>\n";
+    try {
+        if(argc != 5) {
+            std::cerr
+                << "Usage: " << argv[0]
+                << " <recv_port> <send_port> <target_address> <target_port>\n";
             return 1;
         }
         const int recv_port = std::atoi(argv[1]);
         const int send_port = std::atoi(argv[2]);
-        const int target_port = std::atoi(argv[3]);
-        Command command = Command(100);
+        const boost::asio::ip::address target_address =
+            boost::asio::ip::make_address(argv[3]);
+        const int target_port = std::atoi(argv[4]);
+        Command command = Command(100, 100);
         Telemetry telemetry = Telemetry(command);
         boost::asio::io_service io_service;
-        SendServer send_server(io_service, telemetry, command, send_port, target_port, 400);
+        SendServer send_server(io_service, telemetry, command, send_port,
+                               target_address, target_port);
         RecvServer recv_server(io_service, recv_port);
         io_service.run();
-    }
-    catch (std::exception &e)
-    {
+    } catch(std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
 
