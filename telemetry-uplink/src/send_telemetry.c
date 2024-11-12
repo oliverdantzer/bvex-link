6c08e897c81b059ec7ec6860a8dcd9ccceacafe8
Oliver Dantzer <oliverdantzer@gmail.com>
2024-11-12 17:58:01 -0500

    Refactored build_message out of send_sample_async and into send_sample_[type] functions

--- telemetry-uplink/src/send_telemetry.c
+++ telemetry-uplink/src/send_telemetry.c
@@ -1,11 +1,11 @@
 #include "send_telemetry.h"
 #include "generated/nanopb/sample.pb.h"
-#include <pb_encode.h>
-#include <stdio.h>
-#include <string.h>
 #include <arpa/inet.h> // send()
+#include <pb_encode.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum {
     INT32_TYPE,
@@ -30,15 +30,37 @@ int send_sample(int socket_fd, Sample message)
     send(socket_fd, buffer, stream.bytes_written, 0);
 }
 
-void* send_sample_one_arg(void* arg) {
+void* send_sample_one_arg(void* arg)
+{
     send_sample_data_t* data = (send_sample_data_t*)arg;
     send_sample(data->socket_fd, data->sample);
-    free(data);  // Free the allocated data after sending
+    free(data); // Free the allocated data after sending
     return NULL;
 }
 
+int send_sample_async(int socket_fd, Sample sample)
+{
 
-Sample build_message(char* metric_id, float timestamp, void* value, ValueType type) {
+    send_sample_data_t* temp_data = malloc(sizeof(send_sample_data_t));
+    if(temp_data == NULL) {
+        fprintf(stderr, "Memory allocation failed for temp_data.\n");
+        return 1;
+    }
+    temp_data->socket_fd = socket_fd;
+    temp_data->sample = sample;
+    pthread_t temp_thread;
+    int rc = pthread_create(&temp_thread, NULL, send_sample_one_arg, temp_data);
+    if(rc != 0) {
+        fprintf(stderr, "Failed to create temp_thread: %d\n", rc);
+        free(temp_data); // Free temp_data if thread creation fails
+        return 1;
+    }
+    pthread_detach(temp_thread); // Detach thread to let it run independently
+}
+
+Sample build_message(char* metric_id, float timestamp, void* value,
+                     ValueType type)
+{
     Sample message = Sample_init_zero;
     strcpy(message.metric_id, metric_id);
     message.timestamp = timestamp;
@@ -72,58 +94,44 @@ Sample build_message(char* metric_id, float timestamp, void* value, ValueType ty
     return message;
 }
 
-int send_sample_async(int socket_fd, char* metric_id, float timestamp, void* value,
-                ValueType type) {
-    Sample message = build_message(metric_id, timestamp, value, type);
-    send_sample_data_t* temp_data = malloc(sizeof(send_sample_data_t));
-    if (temp_data == NULL) {
-        fprintf(stderr, "Memory allocation failed for temp_data.\n");
-        return 1;
-    }
-    temp_data->socket_fd = socket_fd;
-    temp_data->sample = message;
-    pthread_t temp_thread;
-    int rc = pthread_create(&temp_thread, NULL, send_sample_one_arg, temp_data);
-    if (rc != 0) {
-        fprintf(stderr, "Failed to create temp_thread: %d\n", rc);
-        free(temp_data);  // Free temp_data if thread creation fails
-        return 1;
-    }
-    pthread_detach(temp_thread);  // Detach thread to let it run independently
-}
-
 void send_sample_int32(int socket_fd, char* metric_id, float timestamp,
                        int32_t value)
 {
-    send_sample_async(socket_fd, metric_id, timestamp, &value, INT32_TYPE);
+    Sample sample = build_message(metric_id, timestamp, &value, INT32_TYPE);
+    send_sample_async(socket_fd, sample);
 }
 
 void send_sample_int64(int socket_fd, char* metric_id, float timestamp,
                        int64_t value)
 {
-    send_sample_async(socket_fd, metric_id, timestamp, &value, INT64_TYPE);
+    Sample sample = build_message(metric_id, timestamp, &value, INT64_TYPE);
+    send_sample_async(socket_fd, sample);
 }
 
 void send_sample_float(int socket_fd, char* metric_id, float timestamp,
                        float value)
 {
-    send_sample_async(socket_fd, metric_id, timestamp, &value, FLOAT_TYPE);
+    Sample sample = build_message(metric_id, timestamp, &value, FLOAT_TYPE);
+    send_sample_async(socket_fd, sample);
 }
 
 void send_sample_double(int socket_fd, char* metric_id, float timestamp,
                         double value)
 {
-    send_sample_async(socket_fd, metric_id, timestamp, &value, DOUBLE_TYPE);
+    Sample sample = build_message(metric_id, timestamp, &value, DOUBLE_TYPE);
+    send_sample_async(socket_fd, sample);
 }
 
 void send_sample_bool(int socket_fd, char* metric_id, float timestamp,
                       bool value)
 {
-    send_sample_async(socket_fd, metric_id, timestamp, &value, BOOL_TYPE);
+    Sample sample = build_message(metric_id, timestamp, &value, BOOL_TYPE);
+    send_sample_async(socket_fd, sample);
 }
 
 void send_sample_string(int socket_fd, char* metric_id, float timestamp,
                         char* value)
 {
-    send_sample_async(socket_fd, metric_id, timestamp, value, STRING_TYPE);
+    Sample sample = build_message(metric_id, timestamp, &value, STRING_TYPE);
+    send_sample_async(socket_fd, sample);
 }
