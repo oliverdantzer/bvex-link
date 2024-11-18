a302f2c80b2926df5fc4abdc62fcf0ed1150c503
Oliver Dantzer <oliverdantzer@gmail.com>
2024-11-17 22:58:00 -0500

    Parse target_address arg to 127.0.0.1 if localhost. Use RecvUplinkSampleServer

--- onboard-link/src/main.cpp
+++ onboard-link/src/main.cpp
@@ -1,5 +1,5 @@
 #include "command.hpp"
-#include "recv_server.hpp"
+#include "recv_uplink_sample_server.hpp"
 #include "send_server.hpp"
 #include "telemetry.hpp"
 #include <boost/asio.hpp>
@@ -17,15 +17,22 @@ int main(int argc, char* argv[])
         }
         const int recv_port = std::atoi(argv[1]);
         const int send_port = std::atoi(argv[2]);
-        const boost::asio::ip::address target_address =
-            boost::asio::ip::make_address(argv[3]);
+        std::string target_address_str = argv[3];
+        boost::asio::ip::address target_address;
+        if(target_address_str == "localhost") {
+            // not IPv6 compatible, shouldn't be an issue
+            target_address = boost::asio::ip::make_address("127.0.0.1");
+        } else {
+            target_address = boost::asio::ip::make_address(argv[3]);
+        }
         const int target_port = std::atoi(argv[4]);
+
         Command command = Command(100, 100);
         Telemetry telemetry = Telemetry(command);
         boost::asio::io_service io_service;
         SendServer send_server(io_service, telemetry, command, send_port,
                                target_address, target_port);
-        RecvServer recv_server(io_service, recv_port);
+        RecvUplinkSampleServer recv_server(io_service, command, recv_port);
         io_service.run();
     } catch(std::exception& e) {
         std::cerr << e.what() << std::endl;
