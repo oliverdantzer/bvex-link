f82e135f79475eba8251eb10c91ebf592ebe661b
Oliver Dantzer <oliverdantzer@gmail.com>
2024-11-17 22:53:35 -0500

    Changed SampleData to use variant for different value types instead of subclasses. Removed sample_id from SampleMetadata, that can be added just when downlinking.

--- onboard-link/src/sample.hpp
+++ onboard-link/src/sample.hpp
@@ -1,66 +1,28 @@
-#ifndef SAMPLE_H
-#define SAMPLE_H
+#pragma once
 
 #include <cstdint>
 #include <ctime>
 #include <string>
+#include <variant>
 #include <vector>
 
 typedef std::string MetricId;
 
 struct SampleMetadata {
-    MetricId metric_id;    // Should be unique to each metric
-    int sample_id;    // Should be unique to each metric's samples
-    double timestamp; // Time since last epoch
+    MetricId metric_id; // Should be unique to each metric
+    double timestamp;   // Time since last epoch
 };
+
+typedef std::variant<int32_t, int64_t, float, double, bool, std::string> SampleValue;
 
 class SampleData
 {
   public:
     const SampleMetadata metadata;
-    virtual ~SampleData() = default; // Virtual destructor
+    const SampleValue value;
+    SampleData(std::string metric_id, float timestamp,
+                       SampleValue value);
+    std::string to_json(int sample_id);
+    std::vector<uint8_t> to_bytes(int sample_id);
+    size_t get_size();
 };
-
-class IntSample : public SampleData
-{
-  public:
-    const int value;
-    IntSample(MetricId metric_id, float timestamp, int value);
-};
-
-class LongSample : public SampleData
-{
-  public:
-    const int64_t value;
-    LongSample(MetricId metric_id, float timestamp, int64_t value);
-};
-
-class FloatSample : public SampleData
-{
-  public:
-    float value;
-    FloatSample(MetricId metric_id, float timestamp, float value);
-};
-
-class DoubleSample : public SampleData
-{
-  public:
-    double value;
-    DoubleSample(MetricId metric_id, float timestamp, double value);
-};
-
-class BoolSample : public SampleData
-{
-  public:
-    bool value;
-    BoolSample(std::string metric_id, float timestamp, bool value);
-};
-
-class StringSample : public SampleData
-{
-  public:
-    std::string value;
-    StringSample(std::string metric_id, float timestamp, std::string value);
-};
-
-#endif // SAMPLE_H
