d84553244c213493478a6c1e765290057d085dee
Oliver Dantzer <oliverdantzer@gmail.com>
2024-11-19 23:54:18 -0500

    Refactored and simplified Command class to expose less workings to its consumers.
    Added `SampleDownlinkData` struct to hold data concerning a sample that is to be downlinked.
    
    Added `MetricInfo` struct to hold information about a metric.
    
    Replaced `std::map` with `std::unordered_map` to store metrics for performance.
    
    - Added getter methods (instead of accessing member variables) to `Command` class for managing samples and metrics.

--- onboard-link/src/command.cpp
+++ onboard-link/src/command.cpp
@@ -1,41 +1,112 @@
 #include "command.hpp"
 #include "boost/shared_ptr.hpp"
+#include "sample.hpp"
+#include <iostream>
+#include <optional>
+#include <memory>
 
 Command::Command(size_t bps, size_t max_packet_size_)
-    : bps_(bps), max_packet_size_(max_packet_size_), metric_ids_(),
-      latest_samples_(), metric_shares_() {};
+    : bps_(bps), metrics_() {};
 
 void Command::add_sample(boost::shared_ptr<SampleData> sample)
 {
-    latest_samples_[sample->metadata.metric_id] = sample;
-    metric_ids_.insert(sample->metadata.metric_id);
-    // TODO: Get the share from the ground command instead of automatically
-    // setting it to uniform distribution
-    metric_shares_[sample->metadata.metric_id] = 1.0f / metric_ids_.size();
+    // std::cout << "add_sample. obj memaddr: " << this << std::endl;
+    if(metric_exists(sample->metadata.metric_id)) {
+        metrics_[sample->metadata.metric_id].sample_id++;
+        metrics_[sample->metadata.metric_id].latest_sample = sample;
+    } else {
+
+        // TODO: Get the share values from the ground command instead of this
+        // Set shares to uniform distribution
+        float share = 1.0f / (get_num_metrics() + 1);
+        for (auto& [metric_id, metric_info] : metrics_) {
+            metric_info.share = share;
+        }
+
+        // Create metric_info, populate it with data from sample
+        struct MetricInfo metric_info;
+        metric_info.metric_id = sample->metadata.metric_id;
+        metric_info.share = share;
+        metric_info.latest_sample = sample;
+        // TODO: find more intelligent way of getting max size
+        metric_info.max_sample_size = sample->get_size();
+        metric_info.sample_id = 0;
+
+        // add metric_id:metric_info to map
+        metrics_[sample->metadata.metric_id] = metric_info;
+    }
 }
 
-boost::shared_ptr<SampleData> Command::get_latest_sample(MetricId metric_id)
+std::optional<SampleDownlinkData> Command::pop_latest_sample(MetricId metric_id)
 {
-    return latest_samples_[metric_id];
-}
+    // If metric exists and has sample
+    if(metric_exists(metric_id) &&
+       metrics_[metric_id].latest_sample != nullptr) {
+        // create sample_downlink_data, populate it with
+        // data from latest_sample of metric_id
+        struct SampleDownlinkData sample_downlink_data;
+        sample_downlink_data.sample = metrics_[metric_id].latest_sample;
+        sample_downlink_data.sample_id = metrics_[metric_id].sample_id;
 
-float Command::get_metric_share(MetricId metric_id)
-{
-    return metric_shares_[metric_id];
+        // set latest_sample of metric_id to nullptr,
+        // signifying it is gone
+        metrics_[metric_id].latest_sample = nullptr;
+
+        return sample_downlink_data;
+
+    } else {
+        // return nullopt to signify invalid metric or
+        // no new sample
+        return std::nullopt;
+    }
 }
 
 size_t Command::get_bps() { return bps_; }
 
-const std::set<MetricId>& Command::get_metric_ids() const
-{
-    return metric_ids_;
+std::unique_ptr<std::vector<MetricId>> Command::get_all_metric_ids() {
+    auto metric_ids = std::make_unique<std::vector<MetricId>>();
+
+    // optimization to reserve enough space so we don't need
+    // to continually increase the size of the vector
+    metric_ids->reserve(metrics_.size());
+    
+    for (auto& [metric_id, _]: metrics_) {
+        metric_ids->push_back(metric_id);
+    }
+    return metric_ids;
 }
 
-// std::vector<MetricId> Command::get_metric_ids()
-// {
-//     std::vector<MetricId> metric_ids;
-//     for(auto const& metric_id : metric_ids_) {
-//         metric_ids.push_back(metric_id);
-//     }
-//     return metric_ids;
-// }
+std::optional<MetricInfo> Command::get_metric_info(MetricId metric_id)
+{
+    if(metric_exists(metric_id)) {
+        return metrics_[metric_id];
+    } else {
+        return std::nullopt;
+    }
+}
+
+size_t Command::get_num_metrics() {
+    return metrics_.size();
+}
+
+bool Command::metric_exists(MetricId metric_id)
+{
+    return metrics_.find(metric_id) != metrics_.end();
+}
+
+float Command::get_metric_share(MetricId metric_id)
+{
+    return metrics_[metric_id].share;
+}
+
+size_t Command::get_metric_max_sample_size(MetricId metric_id)
+{
+    return metrics_[metric_id].max_sample_size;
+}
+
+void Command::print_all_metric_ids()
+{
+    for(const auto& metric : metrics_) {
+        std::cout << metric.first << std::endl;
+    }
+}
--- onboard-link/src/command.hpp
+++ onboard-link/src/command.hpp
@@ -2,29 +2,79 @@
 
 #include "sample.hpp"
 #include <boost/shared_ptr.hpp>
-#include <map>
+#include <unordered_map>
+#include <optional>
 #include <set>
 #include <string>
 #include <vector>
+
+/**
+ * @brief Struct to hold data concerning a sample that is to be downlinked
+ */
+struct SampleDownlinkData {
+    // Pointer to the sample's data.
+    boost::shared_ptr<SampleData> sample;
+    // Unique id of the sample (Unique in context of sample's metric)
+    int sample_id;
+};
+
+/**
+ * @brief Struct to hold information about a metric.
+ */
+struct MetricInfo {
+    // Unique of the metric
+    MetricId metric_id;
+    // Amount of bps dedicated to metric telemetry data
+    // Value in range [0-1]
+    float share;
+    // Latest sample of this metric recieved
+    boost::shared_ptr<SampleData> latest_sample;
+    // Sample id of latest sample recieved
+    int sample_id;
+    // Max size in bytes of a sample from this metric
+    size_t max_sample_size;
+};
 
 class Command
 {
   public:
     Command(size_t bps, size_t max_packet_size_);
     // void add_tc_json(const std::string& telecommands_json);
+
     size_t get_bps();
-    size_t get_max_packet_size();
+
+    /**
+     * @brief Adds a sample to the internal data structure.
+     * @param sample Shared pointer to the sample data to be added.
+     */
     void add_sample(boost::shared_ptr<SampleData> sample);
-    boost::shared_ptr<SampleData> get_latest_sample(MetricId metric_id);
+
+    /**
+     * @brief Pop the latest sample data recieved for the given metric ID.
+     * 
+     * Removes sample from internal data structure before returning it.
+     * 
+     * @param metric_id ID of the metric.
+     * @return Optional containing SampleDownlinkData if available.
+     */
+    std::optional<SampleDownlinkData> pop_latest_sample(MetricId metric_id);
+
+    std::optional<MetricInfo> get_metric_info(MetricId metric_id);
+
+    size_t get_num_metrics();
+
     float get_metric_share(MetricId metric_id);
 
-    const std::set<MetricId>& get_metric_ids() const; // Getter for metric_ids
+    void print_all_metric_ids();
+
+    std::unique_ptr<std::vector<MetricId>> get_all_metric_ids();
+
+    bool metric_exists(MetricId metric_id);
+    
+    size_t get_metric_max_sample_size(MetricId metric_id);
 
   private:
-    std::set<MetricId> metric_ids_;
     size_t bps_;
-    size_t max_packet_size_;
     // std::vector<std::string> telecommands_;
-    std::map<MetricId, boost::shared_ptr<SampleData>> latest_samples_;
-    std::map<MetricId, float> metric_shares_;
+    std::unordered_map<MetricId, MetricInfo> metrics_;
 };
