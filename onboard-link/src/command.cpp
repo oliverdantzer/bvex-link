801c3878574b7bb087fb2acc3633b67bfb6382bc
Oliver Dantzer <oliverdantzer@gmail.com>
2025-01-12 22:01:03 -0500

    Updated Command implementation to match header

--- onboard-link/src/command.cpp
+++ onboard-link/src/command.cpp
@@ -4,105 +4,93 @@
 #include <iostream>
 #include <memory>
 #include <optional>
-#include <memory>
 
-Command::Command(size_t bps, size_t max_packet_size_)
-    : bps_(bps), metrics_() {};
+Command::Command(size_t init_bps, size_t init_max_packet_size)
+    : bps_(init_bps), metrics_(), max_packet_size_(init_max_packet_size) {};
 
 void Command::add_sample(boost::shared_ptr<SampleData> sample)
 {
     // std::cout << "add_sample. obj memaddr: " << this << std::endl;
     if(metric_exists(sample->metadata.metric_id)) {
-        metrics_[sample->metadata.metric_id].sample_id++;
-        metrics_[sample->metadata.metric_id].latest_sample = sample;
+        metrics_[sample->metadata.metric_id]->latest_sample = sample;
     } else {
-
-        // TODO: Get the share values from the ground command instead of this
-        // Set shares to uniform distribution
-        float share = 1.0f / (get_num_metrics() + 1);
-        for (auto& [metric_id, metric_info] : metrics_) {
-            metric_info.share = share;
-        }
-
         // Create metric_info, populate it with data from sample
-        struct MetricInfo metric_info;
-        metric_info.metric_id = sample->metadata.metric_id;
-        metric_info.share = share;
-        metric_info.latest_sample = sample;
-        // TODO: find more intelligent way of getting max size
-        metric_info.max_sample_size = sample->get_size();
-        metric_info.sample_id = 0;
+        std::unique_ptr<MetricInfo> metric_info =
+            std::make_unique<MetricInfo>();
+        metric_info->metric_id = sample->metadata.metric_id;
+        metric_info->token_threshold = 1;
+        metric_info->latest_sample = sample;
+        metric_info->sample_transmitter = std::make_unique<SampleTransmitter>(
+            [this, metric_id = sample->metadata.metric_id]() {
+                return pop_latest_sample(metric_id);
+            },
+            [this]() { return get_max_packet_size(); },
+            sample->metadata.metric_id);
 
         // add metric_id:metric_info to map
-        metrics_[sample->metadata.metric_id] = metric_info;
+        metrics_[sample->metadata.metric_id] = std::move(metric_info);
     }
 }
-
-std::optional<SampleDownlinkData> Command::pop_latest_sample(MetricId metric_id)
+boost::shared_ptr<SampleData> Command::pop_latest_sample(MetricId metric_id)
 {
     // If metric exists and has sample
     if(metric_exists(metric_id) &&
-       metrics_[metric_id].latest_sample != nullptr) {
-        // create sample_downlink_data, populate it with
-        // data from latest_sample of metric_id
-        struct SampleDownlinkData sample_downlink_data;
-        sample_downlink_data.sample = metrics_[metric_id].latest_sample;
-        sample_downlink_data.sample_id = metrics_[metric_id].sample_id;
-
-        // set latest_sample of metric_id to nullptr,
-        // signifying it is gone
-        metrics_[metric_id].latest_sample = nullptr;
-
-        return sample_downlink_data;
+       metrics_[metric_id]->latest_sample != nullptr) {
+        boost::shared_ptr<SampleData> sample =
+            metrics_[metric_id]->latest_sample;
+        metrics_[metric_id]->latest_sample = nullptr;
+        return sample;
 
     } else {
-        // return nullopt to signify invalid metric or
+        // return nullptr to signify invalid metric or
         // no new sample
-        return std::nullopt;
+        return nullptr;
+    }
+}
+
+size_t Command::get_max_packet_size() { return max_packet_size_; }
+
+boost::shared_ptr<std::vector<uint8_t>> Command::get_sample_pkt(
+    MetricId metric_id)
+{
+    // If metric exists and has sample
+    if(metric_exists(metric_id) &&
+       metrics_[metric_id]->sample_transmitter != nullptr) {
+        return metrics_[metric_id]->sample_transmitter->get_pkt();
+
+    } else {
+        // return nullptr to signify invalid metric or
+        // no new sample
+        return nullptr;
     }
 }
 
 size_t Command::get_bps() { return bps_; }
 
-std::unique_ptr<std::vector<MetricId>> Command::get_all_metric_ids() {
+std::unique_ptr<std::vector<MetricId>> Command::get_all_metric_ids()
+{
     auto metric_ids = std::make_unique<std::vector<MetricId>>();
 
     // optimization to reserve enough space so we don't need
     // to continually increase the size of the vector
     metric_ids->reserve(metrics_.size());
-    
-    for (auto& [metric_id, _]: metrics_) {
+
+    for(auto& [metric_id, _] : metrics_) {
         metric_ids->push_back(metric_id);
     }
     return metric_ids;
 }
 
-std::optional<MetricInfo> Command::get_metric_info(MetricId metric_id)
-{
-    if(metric_exists(metric_id)) {
-        return metrics_[metric_id];
-    } else {
-        return std::nullopt;
-    }
-}
-
-size_t Command::get_num_metrics() {
-    return metrics_.size();
-}
+size_t Command::get_num_metrics() { return metrics_.size(); }
 
 bool Command::metric_exists(MetricId metric_id)
 {
     return metrics_.find(metric_id) != metrics_.end();
 }
 
-float Command::get_metric_share(MetricId metric_id)
+float Command::get_metric_token_threshold(MetricId metric_id)
 {
-    return metrics_[metric_id].share;
-}
-
-size_t Command::get_metric_max_sample_size(MetricId metric_id)
-{
-    return metrics_[metric_id].max_sample_size;
+    return metrics_[metric_id]->token_threshold;
 }
 
 void Command::print_all_metric_ids()
