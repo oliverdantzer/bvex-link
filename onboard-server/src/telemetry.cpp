c0c0e0dd12f6148d9dc824ca0daa278800e09780
Oliver Dantzer <oliverdantzer@gmail.com>
2025-01-19 18:07:51 -0500

    onboard server: Telemetry: updated to use new MetricIterator definitions

--- onboard-server/src/telemetry.cpp
+++ onboard-server/src/telemetry.cpp
@@ -13,33 +13,36 @@
 Telemetry::Telemetry(Command& command)
     : command_(command), metric_iter_(command.get_metric_iterator()) {};
 
-std::unique_ptr<std::vector<uint8_t>> Telemetry::pop(
-    unsigned int retry_depth)
+std::unique_ptr<std::vector<uint8_t>> Telemetry::pop(unsigned int retry_depth)
 {
-    metric_iter_.increment();
-
-    if(metric_iter_.empty() || retry_depth > command_.get_num_metrics()) {
+    std::optional<SampleInfo> sample = metric_iter_.get_next_metric_sample();
+    if (!sample.has_value()) {
         return nullptr;
     }
 
-    MetricId metric_id = metric_iter_.get_id();
+    std::string metric_id = sample->metric_id;
 
     // if metric id not in metric token counts, add it with initial value
     // of 1
-    if(metric_token_counts_.find(metric_id) == metric_token_counts_.end()) {
+    if(metric_token_counts_.find(metric_id) ==
+       metric_token_counts_.end()) {
         metric_token_counts_[metric_id] = 1;
     }
 
     int& tokens = metric_token_counts_[metric_id];
-    int threshold = metric_iter_.get_token_threshold();
+    int threshold = sample->token_threshold;
     metric_token_counts_[metric_id]++;
     if(tokens >= threshold) {
-        auto payload = metric_iter_.get_sample_pkt();
+        auto payload = sample->get_pkt();
         if(payload != nullptr) {
             tokens = 0;
             return payload;
         }
     }
     tokens++;
-    return pop(retry_depth + 1);
+    if(retry_depth < command_.get_num_metrics()) {
+        return pop(retry_depth + 1);
+    } else {
+        return nullptr;
+    }
 };
