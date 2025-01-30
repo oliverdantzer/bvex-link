cd7992fd68314bf72d52049f9983ab05be91327e
Oliver Dantzer <oliverdantzer@gmail.com>
2025-01-30 14:39:33 -0500

    CHanged example code to inline, added doxygen awesome submodule to properly render css

--- onboard-telemetry-client/include/request.h
+++ onboard-telemetry-client/include/request.h
@@ -1,10 +1,25 @@
 #pragma once
 
-/** @file
+/** @file request.h
  *  @brief API to request telemetry data from the onboard server.
  *
  *  Example usage:
- *  @example example_usage.c
+ *  @code
+ *  #include "request.h"
+ *  int main() {
+ *      Requester requester = make_requester("altitude", "localhost", "3000");
+ *      if (requester.socket_fd < 0) {
+ *          printf("Error creating requester\n");
+ *          return;
+ *      }
+ *      RequestDoubleResult result = request_double(&requester);
+ *      if (result.err) {
+ *          printf("Request error\n");
+ *          return;
+ *      }
+ *      printf("Response value: %f\n", result.value);
+ *  }
+ *  @endcode
  */
 
 #include "generated/nanopb/request.pb.h"
