f52c36031af6da999dec009f2f78489b3f9016cc
Oliver Dantzer <oliverdantzer@gmail.com>
2025-02-05 00:28:47 -0500

    parse ack telecommands

--- onboard-server/src/server/telecommand_recv_server.cpp
+++ onboard-server/src/server/telecommand_recv_server.cpp
@@ -20,11 +20,21 @@ void TelecommandRecvServer::handle_message(
 {
     std::string message_str(message->begin(), message->end());
     json telecommand = json::parse(message_str);
-
-    if(telecommand.contains("set_bps")) {
-        command_.set_bps(telecommand["set_bps"]["value"]);
+    std::cout << telecommand << std::endl;
+    if(telecommand.contains("ack")) {
+        std::vector<int32_t> seqnums_int =
+            telecommand["ack"]["seqnums"].get<std::vector<int32_t>>();
+        std::vector<uint32_t> seqnums(seqnums_int.begin(), seqnums_int.end());
+        Ack ack = {.metric_id = telecommand["ack"]["metric_id"],
+                   .sample_id = telecommand["ack"]["sample_id"],
+                   .seqnums = seqnums};
+        command_.handle_ack(std::move(ack));
+    } else if(telecommand.contains("set_bps")) {
+        command_.set_bps(telecommand["set_bps"]["bps"]);
+        std::cout << "SET BPS" << std::endl;
     } else if(telecommand.contains("set_max_pkt_size")) {
-        command_.set_max_packet_size(telecommand["set_max_pkt_size"]["value"]);
+        command_.set_max_packet_size(
+            telecommand["set_max_pkt_size"]["max_pkt_size"]);
     } else {
         std::cerr << "Telecommand not recognized: " << message_str << std::endl;
     }
