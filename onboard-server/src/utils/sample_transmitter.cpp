44fbb2da0a8694c0b551f3a32f11812abcd91fc7
Oliver Dantzer <oliverdantzer@gmail.com>
2025-02-05 00:30:45 -0500

    sampletransmitter use codec lib

--- onboard-server/src/utils/sample_transmitter.cpp
+++ onboard-server/src/utils/sample_transmitter.cpp
@@ -1,18 +1,20 @@
 #include "sample_transmitter.hpp"
 #include "chunker.hpp"
-#include "encode_downlink_telemetry/encode_sample_frame.hpp"
-#include "encode_downlink_telemetry/size_constants.hpp"
 #include <boost/iterator/counting_iterator.hpp>
+#include <codec/downlink-tm-enc/sample_frame.hpp>
 #include <functional>
 #include <iostream>
 #include <iterator>
 #include <memory>
 #include <vector>
 
+#define IPV4_OVERHEAD 20
+#define UDP_OVERHEAD 20
+
 SampleTransmitter::SampleTransmitter(
-    std::function<std::unique_ptr<SampleData>()> pop_new_sample,
+    std::function<std::shared_ptr<SampleData>()> get_new_sample,
     std::function<size_t()> get_max_pkt_size, MetricId metric_id)
-    : get_new_sample_(pop_new_sample), get_max_pkt_size_(get_max_pkt_size),
+    : get_new_sample_(get_new_sample), get_max_pkt_size_(get_max_pkt_size),
       sample_metadata_({
           .metric_id = metric_id,
           .timestamp = 0.0f,
@@ -21,12 +23,11 @@ SampleTransmitter::SampleTransmitter(
 
 bool SampleTransmitter::set_new_sample()
 {
-    std::unique_ptr<SampleData> sample = get_new_sample_();
+    std::shared_ptr<SampleData> sample = get_new_sample_();
     if(sample == nullptr) {
         return false;
     } else {
-        size_t overhead =
-            SAMPLE_FRAME_OVERHEAD + IPV4_HEADER_OVERHEAD + UDP_HEADER_OVERHEAD;
+        size_t overhead = IPV4_OVERHEAD + UDP_OVERHEAD;
 
         size_t max_segment_size = get_max_pkt_size_() - overhead;
 
@@ -34,7 +35,7 @@ bool SampleTransmitter::set_new_sample()
         sample_metadata_ = sample->metadata;
 
         sample_chunker_ = std::make_unique<Chunker>(
-            Chunker(sample->encode_data(), max_segment_size));
+            Chunker(std::move(sample->encode_data()), max_segment_size));
         unsigned int num_chunks = sample_chunker_->get_num_chunks();
 
         // set all seqnums to unacked
@@ -65,27 +66,27 @@ std::optional<std::vector<uint8_t>> SampleTransmitter::get_pkt()
     unsigned int seq_num = get_itr_val();
     Chunk chunk = sample_chunker_->get_chunk(seq_num);
     increment_itr();
-    SampleFrameData segment_data = {.metadata = sample_metadata_,
+    SampleFrameData segment_data = {.metric_id = sample_metadata_.metric_id,
+                                    .timestamp = sample_metadata_.timestamp,
                                     .data_type = data_type_,
                                     .sample_id = sample_id_,
                                     .num_segments =
                                         sample_chunker_->get_num_chunks(),
                                     .seqnum = seq_num,
                                     .data = std::move(chunk.data)};
-    std::vector<uint8_t> pkt =
-        encode_sample_frame(std::move(segment_data));
+    std::vector<uint8_t> pkt = encode_sample_frame(std::move(segment_data));
 #ifdef DEBUG
-    if(pkt->size() > get_max_pkt_size_()) {
+    if(pkt.size() > get_max_pkt_size_()) {
         std::cerr << "Packet size exceeds maximum packet size" << std::endl;
-        std::cerr << "Actual packet size: " << pkt->size() << std::endl;
+        std::cerr << "Actual packet size: " << pkt.size() << std::endl;
         std::cerr << "Maximum packet size: " << get_max_pkt_size_()
                   << std::endl;
     }
 #endif
     return pkt;
 }
-void SampleTransmitter::handle_ack(
-    const std::vector<uint32_t>& seqnums, SampleId sample_id)
+void SampleTransmitter::handle_ack(const std::vector<uint32_t>& seqnums,
+                                   SampleId sample_id)
 {
     if(sample_id != sample_id_) {
         return;
