784e256ad7385b4aa1350cce9b8bcd80270fc9a1
Oliver Dantzer <oliverdantzer@gmail.com>
2025-02-06 15:45:12 -0500

    refactor CLI class: rename BvexCLI to CLI, add handle_set_pkt_size method, and update command structure

--- command/src/cli.py
+++ command/src/cli.py
@@ -4,12 +4,14 @@ import click
 from click_shell import shell  # type: ignore
 
 
-class BvexCLI:
+class CLI:
     def __init__(
-        self, handle_set_bps: Callable[[int], None], handle_get_bps: Callable[[], int]
+        self,
+        handle_set_bps: Callable[[int], None],
+        handle_set_pkt_size: Callable[[int], None],
     ):
         self.handle_set_bps = handle_set_bps
-        self.handle_get_bps = handle_get_bps
+        self.handle_set_pkt_size = handle_set_pkt_size
         self.app = self.create_app()
 
     def create_app(self):
@@ -23,8 +25,13 @@ class BvexCLI:
             self.handle_set_bps(bits_per_second)
 
         @app.command()
-        def getbps():
-            click.echo(self.handle_get_bps())
+        @click.argument("pkt_size", type=int)
+        def setpktsize(pkt_size: int):
+            self.handle_set_pkt_size(pkt_size)
+
+        # @app.command()
+        # def getbps():
+        #     click.echo(self.handle_get_bps())
 
         return app
 
@@ -32,16 +39,16 @@ class BvexCLI:
         self.app()
 
 
-if __name__ == "__main__":
-    bps = 1
+# if __name__ == "__main__":
+#     bps = 1
 
-    def setbps(bps_):
-        global bps
-        bps = bps_
+#     def setbps(bps_):
+#         global bps
+#         bps = bps_
 
-    def getbps():
-        global bps
-        return bps
+#     def getbps():
+#         global bps
+#         return bps
 
-    cli = BvexCLI(setbps, getbps)
-    cli.run()
+#     cli = CLI(setbps, getbps)
+#     cli.run()
