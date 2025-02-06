7a37af468efacb16b14fde508d7237282f857bd2
Oliver Dantzer <oliverdantzer@gmail.com>
2025-02-05 22:27:30 -0500

    improved sample decode

--- onboard-server/src/codec/onboard-tm/decode.cpp
+++ onboard-server/src/codec/onboard-tm/decode.cpp
@@ -11,24 +11,23 @@
 // returning ptr to SampleData is necessary here because
 // it allows us to use polymorphism and return classes
 // derived from the abstract class SampleData
-std::optional<Sample> decode_payload(
-    std::vector<uint8_t> payload)
+std::optional<Sample> decode_payload(std::vector<uint8_t> payload)
 {
     Sample sample = Sample_init_zero;
-    bool status;
 
     /* Create a stream that reads from the buffer. */
     pb_istream_t stream =
         pb_istream_from_buffer(payload.data(), payload.size());
 
     /* Now we are ready to decode the message. */
-    status = pb_decode(&stream, Sample_fields, &sample);
+    bool success = pb_decode(&stream, Sample_fields, &sample);
 
     /* Check for errors... */
-    if(!status) {
+    if(success) {
+        std::cout << "sample.metric_id: " << sample.metric_id << std::endl;
+        return sample;
+    } else {
         printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
         return std::nullopt;
     }
-
-    return sample;
 }
