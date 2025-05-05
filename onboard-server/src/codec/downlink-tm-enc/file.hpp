6b7a3e5e7bbbde080ee8b428fd4102bd74b9d64f
Oliver Dantzer <oliverdantzer@gmail.com>
2025-05-05 15:30:59 -0400

    delete c++ implementation of onboard server

--- onboard-server/src/codec/downlink-tm-enc/file.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <vector>
-#include <string>
-
-std::vector<uint8_t> encode_file(const std::string& file_path,
-                                 const std::string& extension);
